fd607517183c63750682114239ffa86f3a68f246
Songhyeon Jun <shjeon0730@gmail.com>
2021-07-01 22:29:22 -0500

    add loader

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import mainPage from './pages/MainPage.js';
 const root = document.getElementById('root');
 window.addEventListener('load', () => {
+	for(const node of root.childNodes){
+		root.removeChild(node);
+	}
+	for(const node of root.children){
+		root.removeChild(node);
+	}
 	root.appendChild(mainPage);
 	console.log('loaded');
 });
